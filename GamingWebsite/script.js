ba68551afdb1d46d8da137e3f2da3abe77f0aa82
Musa Shah <shadowvortex604@gmail.com>
2026-06-29 19:50:15 +0500

    Add files via upload

---
+++ GamingWebsite/script.js
@@ -0,0 +1,15 @@
+window.onscroll = function () {
+    const btn = document.getElementById("backToTop");
+    if (window.scrollY > 300) {
+        btn.style.display = "block";
+    } else {
+        btn.style.display = "none";
+    }
+};
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
